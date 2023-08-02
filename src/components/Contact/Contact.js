63f19c020fc5ee5a803e45d372e1a6554e6cfaa6
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2023-08-03 00:38:52 +0500

    react router added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 function Contact() {
+    const navigation = useNavigate()
   return (
-    <div style={{height:'70vh',backgroundColor:'lightgray'}}>Contact Page Component</div>
+    <>
+    <div style={{height:'70vh',backgroundColor:'lightgray'}}>Contact Page Component
+    <br />
+    <button className='btn' onClick={()=> navigation('/')}>Move /</button>
+    </div>
+    
+    </>
   )
 }
 
