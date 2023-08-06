580a349ce0f1ba7567716c1c1827d7703dc77835
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2023-08-06 22:37:52 +0500

    useeffect

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,12 +1,27 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 import { Context } from '../index'
 import Component2 from './Component2'
+import ComputerLanguages from './ComputerLanguages';
 
 function Component1() {
     const data = useContext(Context)
+    const [time,setTime] = useState(new Date());
+    
+    useEffect(() =>{
+     const subscribe = setTimeout(() =>{
+        setTime(new Date())
+       },1000)
+      
+       return () =>{
+       clearTimeout(subscribe)
+       }
+     
+    },[time,setTime])
+
   return (
     <>
     <h1 className='font-bold'>Component1</h1>
+    <h1>{time.toLocaleTimeString()}</h1>
 <p>Passed value by context : {data[1].name}</p>
 
 {
