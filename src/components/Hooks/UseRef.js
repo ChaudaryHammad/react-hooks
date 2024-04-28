84ca62f565e41772418b8450b50842c2e65ef861
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-04-28 22:43:54 +0500

    Project 1

--- src/components/Hooks/UseRef.js
+++ src/components/Hooks/UseRef.js
@@ -3,23 +3,42 @@ import { useRef } from 'react'
 
 const UseRef = () => {
     const [name,setName] = useState('')
-  const renderCount = useRef(0)
-    useEffect(()=>{
-        renderCount.current = renderCount.current + 1
-        
-    
-    })
+    const [image, setImage] = useState([])
+   const inputRef = useRef()
+   console.log(image);
+
+   const fileRef = useRef(null)
 
 
+function focus(){
+    inputRef.current.focus()
+}
 
   return (
   <>
       <div>UseRef Hook</div>
 
-<input type="text" value={name} onChange={(e)=>setName(e.target.value)} name="" id="" />
+<input ref={inputRef} type="text" value={name} onChange={(e)=>setName(e.target.value)}  name="" id="" />
 <h1>{name}</h1>
+<button className='border border-red-600  bg-yellow-600 text-black' onClick={focus}>Focus</button>
 
-<h1>Number of times i render {renderCount.current}</h1>
+
+<input type="file" 
+ref={fileRef}
+accept='image/*'
+
+onChange={(e)=>setImage(e.target.files[0])}
+hidden
+
+/>
+
+<h1 className='cursor-pointer' onClick={()=> fileRef.current.click()}>Set Image</h1>
+
+<h1>{
+    image.length === 0 ? 'No Image' : image.name
+
+}</h1>
+
   </>
   )
 }
