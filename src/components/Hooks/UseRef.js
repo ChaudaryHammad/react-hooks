b717a1f6988c9cce2b8f560534405769d6d48b6f
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-04-28 22:02:02 +0500

    basic usecase

--- src/components/Hooks/UseRef.js
+++ src/components/Hooks/UseRef.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRef } from 'react'
 
 const UseRef = () => {
     const [name,setName] = useState('')
+  const renderCount = useRef(0)
+    useEffect(()=>{
+        renderCount.current = renderCount.current + 1
+        
+    
+    })
+
+
+
   return (
   <>
       <div>UseRef Hook</div>
 
 <input type="text" value={name} onChange={(e)=>setName(e.target.value)} name="" id="" />
 <h1>{name}</h1>
+
+<h1>Number of times i render {renderCount.current}</h1>
   </>
   )
 }
